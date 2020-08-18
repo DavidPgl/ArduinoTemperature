2c1eab58c948400976bee65642aa0f3d9356d2de
DavidPgl <panagiotopulos.d@gmail.com>
2020-08-18 20:36:38 +0200

    [Code for single sensor]

--- arduino_temperature.ino
+++ arduino_temperature.ino
@@ -4,25 +4,22 @@
 #include <DallasTemperature.h>
 
 // Define OneWire with pin-2(Data)
-DallasTemperature sensor1(new OneWire(2));
-DallasTemperature sensor2(new OneWire(4));
+DallasTemperature sensor(new OneWire(2));
 
 DeviceAddress deviceAddress1;
-DeviceAddress deviceAddress2;
 
 struct pt pt1;
-struct pt pt2;
 
-void sendTemperature(float value, int id){
-    Serial.println((String)id +  "&" + (String)value);
+void sendTemperature(float value){
+    Serial.println(value);
 }
 
-int getTemp(struct pt *pt, DallasTemperature sensors, DeviceAddress deviceAddress, int id){
+int getTemp(struct pt *pt, DallasTemperature sensors, DeviceAddress deviceAddress){
     PT_BEGIN(pt);
 
     sensors.requestTemperaturesByAddress(deviceAddress);
     PT_WAIT_UNTIL(pt, sensors.isConversionComplete());
-    sendTemperature(sensors.getTempC(deviceAddress), id);
+    sendTemperature(sensors.getTempC(deviceAddress));
 
     PT_END(pt);
 }
@@ -30,17 +27,12 @@ int getTemp(struct pt *pt, DallasTemperature sensors, DeviceAddress deviceAddres
 void setup() {
     Serial.begin(9600);
 
-    sensor1.begin();
-    sensor1.setWaitForConversion(false);
-    sensor1.getAddress(deviceAddress1, (uint8_t)0);
-    sensor2.begin();
-    sensor2.setWaitForConversion(false);
-    sensor2.getAddress(deviceAddress2, (uint8_t)0);
+    sensor.begin();
+    sensor.setWaitForConversion(false);
+    sensor.getAddress(deviceAddress1, (uint8_t)0);
     delay(1000);
 }
 
 void loop() {
-    getTemp(&pt1, sensor1, deviceAddress1, 0);
-    delay(350);
-    getTemp(&pt2, sensor2, deviceAddress2, 1);
+    getTemp(&pt1, sensor, deviceAddress1);
 }
